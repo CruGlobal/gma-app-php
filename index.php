886cd27655d54e54096ad6c4816ba654eafada3d
Brian Zoetewey <brian.zoetewey@ccci.org>
2015-03-17 16:38:08 -0400

    Use app in iframe

--- index.php
+++ index.php
@@ -2,30 +2,21 @@
 	require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
 	$wrapper = ApplicationWrapper::singleton();
 	$wrapper->authenticate();
-	$min = Config::get( 'use_min', true ) ? '.min' : '';
 	?>
 	<!doctype html>
 	<html>
 	<head>
 		<title>Next-Gen Measurements</title>
 		<link rel="icon" type="image/png" href="<?php echo $wrapper->versionUrl( "app/img/gma-logo.png" ); ?>">
-
-		<link href="<?php echo $wrapper->versionUrl( "app/vendor/bootstrap/dist/css/bootstrap{$min}.css" ); ?>" rel="stylesheet" />
-		<link href="<?php echo $wrapper->versionUrl( "app/vendor/bootstrap/dist/css/bootstrap-theme{$min}.css" ); ?>" rel="stylesheet" />
-		<link href="<?php echo $wrapper->versionUrl( 'app/css/spinner.css' ); ?>" rel="stylesheet" />
-		<link href="<?php echo $wrapper->versionUrl( 'app/css/gcm.css' ); ?>" rel="stylesheet" />
-
-		<script type="application/javascript" src="<?php echo $wrapper->versionUrl( "app/vendor/jquery/dist/jquery{$min}.js" ); ?>"></script>
 		<script type="application/javascript">
 			var gma = window.gma = window.gma || {};
 			gma.config = <?php echo $wrapper->appConfig(); ?>;
 		</script>
+		<script type="application/javascript" src="app/dist/iframeResizer.min.js"></script>
 	</head>
-	<body>
-	<div ng-include="'app/template/app.html'"></div>
-	<script type="application/javascript"
-			data-main="<?php echo $wrapper->versionUrl( "app/js/main.js" ); ?>"
-			src="<?php echo $wrapper->versionUrl( "app/vendor/requirejs/require.js" ); ?>"></script>
+	<body style="margin: 0;">
+	<iframe id="GlobalMeasurementsApplication" src="app/dist/gma.html" style="width: 100%; border-width: 0;" scrolling="no"></iframe>
+	<script type="application/javascript">iFrameResize( {}, document.getElementById( 'GlobalMeasurementsApplication' ) );</script>
 	</body>
 	</html>
 <?php }
