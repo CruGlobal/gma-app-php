452f050e3e5c2258c2c9d0aaa3c66c90e388f5af
Mike Albert <mike.albert@cru.org>
2016-12-12 14:44:32 -0500

    Switch to ECR for docker images

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git submodule update --init --recursive
-docker build -t cruglobal/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER .
+docker build -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER .
