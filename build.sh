f8c2937aa11d7ab03484b1700cea8a181598a279
Josh Starcher <josh.starcher@gmail.com>
2017-10-26 13:35:36 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 git submodule update --init --recursive
 
 docker build \
-  -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER .
+  -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$ENVIRONMENT-$BUILD_NUMBER .
 rc=$?
 
 if [ $rc -ne 0 ]; then
